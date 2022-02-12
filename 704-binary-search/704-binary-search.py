f165e7d3da9e29cc4bdf66108a14053a8dbb9fb3
byte19 <75004559+byte19@users.noreply.github.com>
2022-02-12 13:05:49 +0530

    Time: 213 ms (62.19%), Space: 14.7 MB (27.81%) - LeetHub

---
+++ 704-binary-search/704-binary-search.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def search(self, nums, target):
+        
+        l = 0
+        r = len(nums)-1
+        
+        while l <= r:
+            
+            m = (l+r)//2
+            if nums[m] == target:
+                return m
+            
+            if nums[m] > target:
+                r = m-1
+            
+            if nums[m] < target:
+                l = m+1
+        
+        return -1
