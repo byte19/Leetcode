dee91bfa3c65b75dd53dba26ded56b673b28f4cc
byte19 <75004559+byte19@users.noreply.github.com>
2022-02-13 12:45:22 +0530

    Time: 32 ms (95.56%), Space: 13.6 MB (74.15%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def commonChars(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        words_len = len(words)
+        if words_len == 1:
+            return list(words[0])
+
+        letters = list(words[0])
+        check_words = words[1:]
+        words_len -= 1
+
+        result = ""
+        for l in letters:
+            letter_in_all_word = False
+            for word_index in range(words_len):
+                if l in check_words[word_index]:
+                    letter_in_all_word = True
+                    check_words[word_index] = check_words[word_index].replace(l, "", 1)
+                else:
+                    letter_in_all_word = False
+                    break
+            if letter_in_all_word:
+                result += l
+
+        return list(result)
