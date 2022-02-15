455e5044e6c8cf129925b6b03dcd57bc394a903d
byte19 <75004559+byte19@users.noreply.github.com>
2022-02-15 19:05:34 +0530

    Time: 169 ms (49.29%), Space: 15.6 MB (68.66%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,13 +1,8 @@
 class Solution(object):
     def singleNumber(self, nums):
-        dict = {}
-        for num in nums:
-            if num not in dict.keys():
-                dict[num]=1
-            else:
-                dict[num]+=1
         
+        r = 0
         for num in nums:
-            if dict[num]==1:
-                return num
-        
+            r = r^num
+            
+        return r
