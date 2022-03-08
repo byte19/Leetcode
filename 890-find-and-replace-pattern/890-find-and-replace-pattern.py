1b5015a26cc07c33c569754d5022806eff979cd5
byte19 <75004559+byte19@users.noreply.github.com>
2022-03-08 22:00:43 +0530

    Time: 24 ms (67.35%), Space: 13.5 MB (45.92%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findAndReplacePattern(self, words, pattern):
+        """
+        :type words: List[str]
+        :type pattern: str
+        :rtype: List[str]
+        """
+        def encode(patt, word):
+            occ=[None for i in range(26)]
+            for i in range(len(word)):
+                if occ[ord(patt[i])-ord('a')]==None:
+                    occ[ord(patt[i])-ord('a')] = word[i]
+                else:
+                    if occ[ord(patt[i])-ord('a')] !=word[i]:
+                        return False
+            return True
+        res=[]
+        for word in words:
+            if encode(pattern, word) and encode(word, pattern):
+                res.append(word)
+        return res
+                
