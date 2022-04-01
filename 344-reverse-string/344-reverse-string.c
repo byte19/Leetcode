c8483ea270e3f9671850579eb03404c0b4fcf5b6
byte19 <75004559+byte19@users.noreply.github.com>
2022-04-01 10:41:34 +0530

    Time: 44 ms (97.93%), Space: 12.6 MB (19.67%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.c
@@ -0,0 +1,14 @@
+
+
+void reverseString(char* s, int sSize){
+    int i = 0;
+    int j = sSize-1;
+    while(i<j){
+        char temp = s[i];
+        s[i]=s[j];
+        s[j]=temp;
+        j--;
+        i++;
+    }
+    return s;
+}
