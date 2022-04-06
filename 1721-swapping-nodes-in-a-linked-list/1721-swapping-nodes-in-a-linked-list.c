9c9acf7605177860c3e8c06c4c4059efd529c4af
byte19 <75004559+byte19@users.noreply.github.com>
2022-04-06 11:35:00 +0530

    Time: 216 ms (99.02%), Space: 48.4 MB (52.46%) - LeetHub

---
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.c
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* swapNodes(struct ListNode* head, int k){
+    
+    struct ListNode* temp = head;
+    int len=0;
+    while(temp){
+        len++;
+        temp = temp->next;
+    }
+    struct ListNode* beg;
+    struct ListNode* end;
+    int starti = k-1;
+    int endi = len-k;
+    int i=0;
+    temp = head;
+    while(temp){
+        if(i==starti)
+        beg = temp;
+        
+        if(i==endi)
+        end = temp;
+        
+        temp = temp->next;
+        i++;
+    }
+    
+    int tempo;
+    tempo = beg->val;
+    beg->val = end->val;
+    end->val = tempo;
+    
+    return head;
+
+}
