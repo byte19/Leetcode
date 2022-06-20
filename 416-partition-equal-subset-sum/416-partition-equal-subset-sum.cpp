9f0293ac203a243ca732f2473f192125f5bba261
byte19 <75004559+byte19@users.noreply.github.com>
2022-06-20 09:45:02 +0530

    Time: 392 ms (30.60%), Space: 11.9 MB (58.94%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -4,34 +4,23 @@ public:
         int total = 0;
         int n = nums.size();
         for(int i=0;i<n;i++) total+=nums[i];
-        
-        if(total%2==1) return false;
-        else{
-        int k = total/2;
-        
-        vector<bool> prev(k+1,false);
-    
-        prev[0] = true;
-        
-        if(nums[0]<=k)
-            prev[nums[0]] = true;
-        
-        for(int ind = 1; ind<n; ind++){
-            vector<bool> cur(k+1,false);
-            cur[0] = true;
-            for(int target= 1; target<=k; target++){
-                bool notTaken = prev[target];
-        
-                bool taken = false;
-                    if(nums[ind]<=target)
-                        taken = prev[target-nums[ind]];
+        if(total%2 == 0 && n>1){
+            int target = total/2;
+            vector<vector<bool>> dp(n, vector<bool>(target+1,0));
+            for(int i=0;i<n;i++) dp[i][0] = true;
+            if(target>=nums[0]) dp[0][nums[0]] = true;
             
-                cur[target]= notTaken||taken;
+            for(int ind=1;ind<n;ind++){
+                for(int k=1;k<=target;k++){
+                    bool not_take = dp[ind-1][k];
+                    bool take = false;
+                    if(k >= nums[ind]) take = dp[ind-1][k-nums[ind]];
+                    
+                    dp[ind][k] = take || not_take;
+                }
             }
-            prev = cur;
+            return dp[n-1][target];
         }
-        
-        return prev[k];
-        }
+        return false;
     }
 };
