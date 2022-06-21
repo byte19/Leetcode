17585dcbb9088db5b21f34425191bd03b87340ba
byte19 <75004559+byte19@users.noreply.github.com>
2022-06-21 14:41:48 +0530

    Time: 108 ms (65.80%), Space: 45.7 MB (17.25%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve(int ind,vector<int>& coins,int amount,vector<vector<int>>& dp){
+        if(ind==0){
+            if(amount%coins[0]==0) return amount/coins[0];
+            else return 1e9;
+        }
+        if(dp[ind][amount]!=-1) return dp[ind][amount];
+        
+        int not_take = 0 + solve(ind-1,coins,amount,dp);
+        int take = INT_MAX;
+        if(coins[ind]<=amount) take = 1 + solve(ind,coins,amount-coins[ind],dp);
+        
+        return dp[ind][amount] = min(not_take,take);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1 , -1));
+        long long int x = solve(n-1,coins,amount,dp);
+        if(x==1e9) return -1;
+        else return x;
+        
+    }
+};
