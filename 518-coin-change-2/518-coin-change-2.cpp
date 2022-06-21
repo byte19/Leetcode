5589d6f2d2ea48099339a3e57dd90d27d8059722
byte19 <75004559+byte19@users.noreply.github.com>
2022-06-21 19:55:02 +0530

    Time: 56 ms (32.79%), Space: 18.5 MB (40.14%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int solve(int ind, vector<int>& coins, int amount,vector<vector<int>>& dp){
-        if(ind==0){
-            if(amount%coins[0] == 0) return 1;
-            else return 0;
-        }
-        
-        if(dp[ind][amount]!=-1) return dp[ind][amount];
-        
-        int notTake = solve(ind-1,coins,amount,dp);
-        int take = 0;
-        if(amount>=coins[ind]) take = solve(ind,coins,amount - coins[ind],dp);
-        
-        return dp[ind][amount] = take + notTake;
-    }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1, -1));
-        return solve(n-1,coins,amount,dp);
+        vector<vector<int>> dp(n,vector<int>(amount+1, 0));
+        //return solve(n-1,coins,amount,dp);
+        
+        for(int T=0;T<=amount;T++){
+            if(T%coins[0]==0) dp[0][T] = 1;
+        }
+        
+        for(int ind=1;ind<n;ind++){
+            for(int am=0;am<=amount;am++){
+                int notTake = dp[ind-1][am];
+                int take = 0;
+                if(am>=coins[ind]) take = dp[ind][am - coins[ind]];
+                
+                dp[ind][am] = take + notTake;
+            }
+        }
+        return dp[n-1][amount];
     }
 };
