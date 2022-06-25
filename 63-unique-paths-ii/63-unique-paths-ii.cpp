f5a05b26c7c400a3c474b04e5ac58e0c6f334de5
byte19 <75004559+byte19@users.noreply.github.com>
2022-06-25 17:45:12 +0530

    Time: 3 ms (83.22%), Space: 7.9 MB (42.81%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>>& obstacleGrid,vector<vector<int>>& dp){
-        if(i<0 || j<0) return 0;
-        if(obstacleGrid[i][j]==1) return 0;
-        if(i==0 && j==0) return 1;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        int up = solve(i-1,j,obstacleGrid,dp);
-        int left = solve(i,j-1,obstacleGrid,dp);
-        return dp[i][j] = up+left;
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         
         vector<vector<int>> dp(m, vector<int>(n, 0));
-        //return solve(m-1,n-1,obstacleGrid,dp);
-        
+        vector<int> prev(n,0),curr(n,0);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(obstacleGrid[i][j]==1) dp[i][j]=0;
-                else if(i==0 && j==0) dp[i][j]=1;
+                if(obstacleGrid[i][j]==1) curr[j]=0;
+                else if(i==0 && j==0) curr[j]=1;
                 else{
                     int up=0,left=0;
-                    if(i>0) up = dp[i-1][j];
-                    if(j>0) left = dp[i][j-1];
-                    dp[i][j] = up+left;
+                    if(i>0) up = prev[j];
+                    if(j>0) left = curr[j-1];
+                    curr[j] = up+left;
                 }
             }
+            prev = curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
