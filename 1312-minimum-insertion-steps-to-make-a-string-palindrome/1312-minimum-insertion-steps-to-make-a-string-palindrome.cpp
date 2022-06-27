cccfcbdb0f05dfc994dd9fe34853f2198b194ef6
byte19 <75004559+byte19@users.noreply.github.com>
2022-06-27 22:17:04 +0530

    Time: 113 ms (23.48%), Space: 27.2 MB (25.27%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -3,7 +3,9 @@ public:
     int solve(int i,int j,string& s,vector<vector<int>> &dp){
         if(i>j) return 0;
         if(i==j) return 1;
+        
         if(dp[i][j]!=-1) return dp[i][j];
+        
         if(s[i]==s[j]) return dp[i][j] = 2 + solve(i+1,j-1,s,dp);
         else return dp[i][j] = max(solve(i+1,j,s,dp),solve(i,j-1,s,dp));
     }
