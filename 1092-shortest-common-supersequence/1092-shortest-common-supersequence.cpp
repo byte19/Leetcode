3cb2294a7271ef131e7fea864329987e6153a67d
byte19 <75004559+byte19@users.noreply.github.com>
2022-06-28 13:54:00 +0530

    Time: 24 ms (62.05%), Space: 12.9 MB (50.30%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -20,26 +20,13 @@ public:
             
             if(str1[i-1]==str2[j-1]){
                 ans+=str1[i-1];
-                i--;
-                j--;
+                i--,j--;
             }
-            else if(dp[i-1][j]>dp[i][j-1]){
-                ans+=str1[i-1];
-                i--;
-            }
-            else{
-                ans+=str2[j-1];
-                j--;
-            }
+            else if(dp[i-1][j]>dp[i][j-1]) ans+=str1[i-1],i--;
+            else ans+=str2[j-1],j--;
         }
-        while(i>0){
-            ans+=str1[i-1];
-            i--;
-        }
-        while(j>0){
-            ans+=str2[j-1];
-            j--;
-        }
+        while(i>0) ans+=str1[i-1],i--;
+        while(j>0) ans+=str2[j-1],j--;
         
         reverse(ans.begin(), ans.end());
         return ans;
