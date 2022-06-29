4fb3276fd47d7657dc0462f03f8b5dc960f75b61
byte19 <75004559+byte19@users.noreply.github.com>
2022-06-29 19:05:06 +0530

    Time: 4 ms (46.79%), Space: 9.3 MB (47.00%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
@@ -1,33 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 #include<bits/stdc++.h>
+
 class Solution {
 public:
-    int res = 0;
-    
-    void solve(TreeNode* root,string s){
-        if(root->left==NULL && root->right==NULL){
-            s += to_string(root->val);
-            res += stoi(s);
-            return;
-        }
-        
-        if(root->left) solve(root->left,s+to_string(root->val));
-        if(root->right) solve(root->right,s+to_string(root->val));
-    }
-    
-    int sumNumbers(TreeNode* root) {
-        string s = "";
-        solve(root,s);
-        return res;
-    }
+	int res = 0;
+	
+	void solve(TreeNode* root,string s){
+		if(root->left==NULL && root->right==NULL){
+			s += to_string(root->val);  //converting int to string so that we can append leaf node value
+			res += stoi(s);    //converting string to int
+			return;
+		}
+		
+		if(root->left) solve(root->left,s+to_string(root->val));
+		if(root->right) solve(root->right,s+to_string(root->val));
+	}
+	
+	int sumNumbers(TreeNode* root) {
+		string s = "";
+		solve(root,s);
+		return res;
+	}
 };
