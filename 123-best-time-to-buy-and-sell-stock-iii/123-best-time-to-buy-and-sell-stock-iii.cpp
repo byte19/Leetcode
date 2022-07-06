5deecfd4b1974099fdde09c1300fa7ffdedef9bd
byte19 <75004559+byte19@users.noreply.github.com>
2022-07-06 12:10:35 +0530

    Time: 380 ms (44.31%), Space: 75.3 MB (76.58%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,42 +1,26 @@
 class Solution {
 public:
-    int solve(int ind,vector<int>& prices,int N,int buy,int trans,vector<vector<vector<int>>>& dp){
-        if(ind==N) return 0;
-        
-        if(trans==2) return 0;
-        
-        long profit = 0;
-        
-        if(dp[ind][buy][trans]!=-1) return dp[ind][buy][trans];
-        if(buy==0){
-            profit = max(-prices[ind] + solve(ind+1,prices,N,1,trans,dp),solve(ind+1,prices,N,0,trans,dp));
-        }
-        else{
-            profit = max(prices[ind] + solve(ind+1,prices,N,0,trans+1,dp),solve(ind+1,prices,N,1,trans,dp));
-        }
-        
-        return dp[ind][buy][trans] = profit;
-    }
     int maxProfit(vector<int>& prices) {
         int N = prices.size();
-        vector<vector<vector<int>>> dp(N+1,vector<vector<int>>(2,vector<int>(3,0)));
-        //return solve(0,prices,N,0,0,dp);
+        //vector<vector<vector<int>>> dp(N+1,vector<vector<int>>(2,vector<int>(3,0)));
+        vector<vector<int>> prev(2,vector<int>(3,0)),curr(2,vector<int>(3,0));
         for(int ind=N-1;ind>=0;ind--){
             for(int buy=0;buy<=1;buy++){
                 for(int trans=0;trans<2;trans++){
                     long profit = 0;
         
                     if(buy==0){
-                        profit = max(-prices[ind] + dp[ind+1][1][trans] ,dp[ind+1][0][trans]);
+                        profit = max(-prices[ind] + prev[1][trans] ,prev[0][trans]);
                     }
                     else{
-                        profit = max(prices[ind] + dp[ind+1][0][trans+1] ,dp[ind+1][1][trans]);
+                        profit = max(prices[ind] + prev[0][trans+1] ,prev[1][trans]);
                     }
                     
-                    dp[ind][buy][trans] = profit;
+                    curr[buy][trans] = profit;
                 }
             }
+            prev = curr;
         }
-        return dp[0][0][0];
+        return prev[0][0];
     }
 };
