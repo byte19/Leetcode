65ab71485881c77948e896ffe8a19228d4487d5a
byte19 <75004559+byte19@users.noreply.github.com>
2022-07-06 11:35:02 +0530

    Time: 11 ms (54.11%), Space: 15.2 MB (14.26%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
-    int solve(int ind,vector<int>& prices,bool bought,int N,vector<vector<int>>& dp){
-        if(ind==N) return 0;
-        
-        
-        
-        if(dp[ind][bought]!=-1) return dp[ind][bought];
-        long profit = 0;
-        
-        if(bought==false){
-            profit = max(-prices[ind] + solve(ind+1,prices,true,N,dp),solve(ind+1,prices,false,N,dp));
-        }
-        if(bought==true){
-            profit = max(prices[ind] + solve(ind+1,prices,false,N,dp),solve(ind+1,prices,true,N,dp));
-        }
-        
-        return dp[ind][bought] = profit;
-    }
     int maxProfit(vector<int>& prices) {
         int N = prices.size();
         vector<vector<int>> dp(N+1,vector<int>(2,0));
-        //return solve(0,prices,false,N,dp);
+        vector<long> prev(2,0),curr(2,0);
         
         for(int ind=N-1;ind>=0;ind--){
             for(int bought=0;bought<=1;bought++){
                 long profit = 0;
                 if(bought){
-                    profit = max(-prices[ind] + dp[ind+1][0],dp[ind+1][1]);
+                    profit = max(-prices[ind] + prev[0],prev[1]);
                 }
                 else{
-                    profit = max(prices[ind] + dp[ind+1][1],dp[ind+1][0]);
+                    profit = max(prices[ind] + prev[1],prev[0]);
                 }
                 
-               dp[ind][bought] = profit;
+               curr[bought] = profit;
             }
+            prev = curr;
         }
-        return dp[0][1];
+        return prev[1];
     }
 };
