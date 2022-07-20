bff292cd6740f31e941fb7dcfeb34976cdfcab39
byte19 <75004559+byte19@users.noreply.github.com>
2022-07-20 10:56:48 +0530

    Time: 270 ms (72.19%), Space: 34.3 MB (96.83%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool solve(string& s1,string &s2){
+        int i = 0;
+        int j = 0;
+        
+        while(i<s1.size() && j<s2.size()){
+            if(s1[i]==s2[j]){
+                i++;
+                j++;
+            }
+            if(s1[i]!=s2[j]) i++;
+            
+        }
+        if(j==s2.size()) return true;
+        return false;
+    }
+    int numMatchingSubseq(string s, vector<string>& words) {
+        int wordLen = words.size();
+        unordered_map<string,bool> hash;
+        int count = 0;
+        for(int i=0;i<wordLen;i++){
+            int m = s.size();
+            if(hash.count(words[i]) == 0){
+                if(solve(s,words[i])) {
+                    hash[words[i]] = true;
+                    count++;
+                }
+                else hash[words[i]] = false;
+            }
+            else{
+                if(hash[words[i]]==true) count++;
+            }
+        }
+        return count;
+    }
+};
