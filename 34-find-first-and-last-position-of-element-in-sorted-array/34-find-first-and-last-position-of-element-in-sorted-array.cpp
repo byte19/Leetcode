4420b85f8a7c21e55df0c53092ad3a409154dd19
byte19 <75004559+byte19@users.noreply.github.com>
2022-07-25 11:41:51 +0530

    Time: 0 ms (100.00%), Space: 13.8 MB (17.99%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int binarySearch(vector<int>& nums,int n,int target) {
+        int p=0,q=n-1;
+        while(p<=q) {
+            int mid = (p+q)/2;
+            
+            if(nums[mid]==target) return mid;
+            if(nums[mid] > target) q = mid-1;
+            if(nums[mid] < target) p = mid+1;
+        }
+        return -1;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if(nums.size()==1) {
+            if(nums[0]==target) return {0,0};
+            else return {-1,-1};
+        }
+        int ind = binarySearch(nums,nums.size(),target);
+        if(ind==-1) return {-1,-1};
+        else {
+            vector<int> res {ind,ind};
+            int start = ind,end = ind;
+            while(1) {
+                bool stopped = true;
+                if(start>=0 && nums[start]==target) {
+                    res[0]=start;
+                    start-=1;
+                    stopped=false;
+                }
+                if(end < nums.size() && nums[end]==target) {
+                    res[1]=end;
+                    end+=1;
+                    stopped=false;
+                }
+                if(stopped) return res;
+            }
+        }
+    }
+};
