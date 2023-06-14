6e29a02ff696c9c1df7e2d6eccccdad5e4ac5555
byte19 <75004559+byte19@users.noreply.github.com>
2023-06-14 18:16:13 +0530

    Added solution - LeetHub

---
+++ Find the element that appears once in sorted array - GFG/find-the-element-that-appears-once-in-sorted-array.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Driver code
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    int findOnce(int a[], int n)
+    {
+        //code here.
+        if(n==0) return a[0];
+        
+        for(int i=0;i<n-1;i+=2) {
+            if(a[i]!=a[i+1]) return a[i];
+        }
+        return a[n-1];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int A[n];
+        for(int i = 0;i < n;i++)
+        {
+            cin>>A[i];
+        }
+        
+        Solution ob;
+        
+        int res = ob.findOnce(A,n);
+        cout << res << endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
