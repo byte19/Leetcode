00f409dfed99fde8ea03e070e770f3e7305863d4
byte19 <75004559+byte19@users.noreply.github.com>
2023-08-05 10:53:50 +0530

    Added solution - LeetHub

--- Chocolate Distribution Problem - GFG/chocolate-distribution-problem.cpp
+++ Chocolate Distribution Problem - GFG/chocolate-distribution-problem.cpp
@@ -6,7 +6,6 @@ using namespace std;
 class Solution{
     public:
     long long findMinDiff(vector<long long> a, long long n, long long m){
-    //code
     sort(a.begin(),a.end());
     long long mini = 1e9;
     long long i = 0,j=m-1;
