5f6013d6d4066c03f2b4da326f85feec186d14c6
byte19 <75004559+byte19@users.noreply.github.com>
2023-08-15 12:59:44 +0530

    Added solution - LeetHub

--- Restrictive Candy Crush - GFG/restrictive-candy-crush.cpp
+++ Restrictive Candy Crush - GFG/restrictive-candy-crush.cpp
@@ -11,7 +11,7 @@ class Solution{
         // Your code goes here
         if(k==1) return "";
         stack<pair<char,int>> st;
-        for(int i=0;i<s.size();i++) {
+        for(int i=s.size()-1;i<s.size();i--) {
             if(st.empty() || st.top().first!=s[i]) st.push({s[i],1});
             else if(st.top().first == s[i]) {
                 st.push({s[i],st.top().second+1});
@@ -26,7 +26,7 @@ class Solution{
             ans+=st.top().first;
             st.pop();
         }
-        reverse(ans.begin(),ans.end());
+        // reverse(ans.begin(),ans.end());
         return ans;
     }
 
